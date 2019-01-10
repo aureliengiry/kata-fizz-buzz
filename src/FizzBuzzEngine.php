5a807878b386968b7e3fe0d79624bbd8ebd49c87
Aurélien GIRY <aurelien.giry@gmail.com>
2019-01-10 13:56:10 +0100

    FizzBuzz: test one return one

--- src/FizzBuzzEngine.php
+++ src/FizzBuzzEngine.php
@@ -5,5 +5,8 @@ namespace Kata;
 
 class FizzBuzzEngine
 {
-
+    public function run(int $value)
+    {
+        return 1;
+    }
 }
--- tests/FizzBuzzEngineTest.php
+++ tests/FizzBuzzEngineTest.php
@@ -7,4 +7,20 @@ use Kata\FizzBuzzEngine;
 class FizzBuzzEngineTest extends TestCase
 {
 
+    /** @var FizzBuzzEngine */
+    protected $fizzBuzzEngine;
+
+    protected function setUp()
+    {
+        $this->fizzBuzzEngine = new FizzBuzzEngine();
+    }
+
+    /**
+     * @test
+     */
+    public function oneReturnOne(){
+        $this->assertSame(1, $this->fizzBuzzEngine->run(1));
+    }
 }
+
+
